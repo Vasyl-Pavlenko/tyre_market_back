ff83dd82d6642f7b25356f3489c3b9af29b6a32e
unknown <genlus@ukr.net>
2025-05-30 10:44:19 +0300

    Cloudinary logic changed

--- server.js
+++ server.js
@@ -33,33 +33,38 @@ const upload = multer({
 app.post('/upload', upload.single('image'), async (req, res) => {
   try {
     const file = req.file;
+
     if (!file) {
       return res.status(400).json({ message: 'Файл не передано' });
     }
 
-    const uploadImageToCloudinary = () =>
-      new Promise((resolve, reject) => {
-        const stream = cloudinary.uploader.upload_stream(
-          {
-            folder: 'tyres',
-            format: 'webp',
-            transformation: [{ width: 1200, crop: 'limit' }],
-          },
-          (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          },
-        );
-        streamifier.createReadStream(file.buffer).pipe(stream);
-      });
+    const result = await new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder: 'tyres',
+          format: 'webp',
+          transformation: [{ width: 1600, crop: 'limit' }],
+        },
+        (error, result) => {
+          if (error) {
+            return reject(error);
+          }
 
-    const result = await uploadImageToCloudinary();
+          resolve(result);
+        },
+      );
+
+      streamifier.createReadStream(req.file.buffer).pipe(uploadStream);
+    });
 
     res.status(200).json({
-      image: {
-        width: result.width,
-        url: result.secure_url,
-      },
+      images: [
+        {
+          width: result.width,
+          url: result.secure_url,
+          public_id: result.public_id,
+        },
+      ],
     });
   } catch (error) {
     console.error('Cloudinary upload error:', error);
